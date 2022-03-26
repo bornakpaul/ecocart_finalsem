98a5f0ef034d9e71899d431851b4eb0dee77d407
Bornak <bornakpaul@gmail.com>
2022-03-26 20:30:20 +0530

    New screens created

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,10 +1,57 @@
+import 'package:ecocart/ui/cart_screen.dart';
+import 'package:ecocart/ui/common/color_theme.dart';
+import 'package:ecocart/ui/controllers/main_screen_controllers.dart';
+import 'package:ecocart/ui/home_screen.dart';
+import 'package:ecocart/ui/settings_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GetBuilder<MainController>(builder: (controller) {
+      return Scaffold(
+        body: IndexedStack(
+          index: controller.tabIndex,
+          children: const [
+            HomeScreen(),
+            CartScreen(),
+            SettingsScreen(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: ColorSystem.of(context).primaryColor,
+          elevation: 0.0,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white60,
+          selectedLabelStyle: const TextStyle(fontSize: 16),
+          unselectedLabelStyle: const TextStyle(fontSize: 16),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings_solid),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: controller.tabIndex,
+          onTap: controller.changeTabIndex,
+        ),
+      );
+    });
   }
 }
